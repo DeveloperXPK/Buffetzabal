f7512b57699e77cd593753d6f704ae5e209a091d
Andres Giraldo <andresgiraldo05a@gmail.com>
2024-11-24 13:51:01 -0500

    Corrige referencias de esquema en el modelo de comentarios

--- Backend/models/comentarios.js
+++ Backend/models/comentarios.js
@@ -4,20 +4,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const comentarioSchema = new Schema({
+
+    // Asociamos el comentario con el usuario que lo realizo
     usuario: {
-        _id: { type: Schema.Types.ObjectId, ref: 'Usuario' },
+        _id: { type: Schema.Types.ObjectId, ref: 'usuarios' },
         nombre: String,
         email: String
     },
+
+    // Asociamos el comentario con la publicacion a la que pertenece
     publicacion: {
-        _id: { type: Schema.Types.ObjectId, ref: 'Plato' },
+        _id: { type: Schema.Types.ObjectId, ref: 'platos' },
         nombre: String,
         descripcion: String
     },
+
+    // Definimos el contenido del comentario
     comentario: {
         type: String,
         required: true
     },
+
+    // Definimos la fecha de creacion del comentario
     fecha: {
         type: Date,
         default: Date.now
