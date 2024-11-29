b893595b55c0084a17a5d872701470aded2efd31
Andres Giraldo <andresgiraldo05a@gmail.com>
2024-11-29 12:14:47 -0500

    Añade componente para crear un nuevo plato y actualiza el encabezado para mostrar la opción de agregar plato para administradores

---
+++ Frontend/src/app/components/crear-plato/crear-plato.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PlatosService } from '../../services/platos.service';
+import { Router } from '@angular/router';
+import { AutenticacionService } from '../../services/autenticacion.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-crear-plato',
+  standalone: true,
+  imports: [FormsModule],
+  template: `
+    <div class="container mt-5">
+      <h2>Crear Plato</h2>
+      <form>
+        <div class="mb-3">
+          <label for="nombre" class="form-label">Nombre del Plato</label>
+          <input
+            type="text"
+            name="nombre"
+            class="form-control"
+            [(ngModel)]="nombre"
+            placeholder="Ej. Jugo de maracuyá"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="precio" class="form-label">Precio</label>
+          <input
+            type="number"
+            name="precio"
+            class="form-control"
+            [(ngModel)]="precio"
+            placeholder="Ej. 18000"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="descripcion" class="form-label">Descripción</label>
+          <textarea
+            name="descripcion"
+            class="form-control"
+            [(ngModel)]="descripcion"
+            placeholder="Describe el plato"
+          ></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="categoria" class="form-label">Categoría</label>
+          <select name="categoria" class="form-control" [(ngModel)]="categoria">
+            <option value="">Seleccionar</option>
+            <option value="Entrada">Entradas</option>
+            <option value="Bebida">Bebidas</option>
+            <option value="Postre">Postres</option>
+            <option value="Plato fuerte">Plato fuerte</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary" (click)="crearPlato()">Crear Plato</button>
+      </form>
+    </div>
+  `,
+})
+export class CrearPlatoComponent {
+  public nombre: string = '';
+  public precio: string = '';
+  public descripcion: string = '';
+  public categoria: string = '';
+  public imagen: string = 'default';
+
+  constructor(
+    private platoService: PlatosService,
+    private router: Router,
+    private autenticacion: AutenticacionService,
+    private http: HttpClient
+  ) {}
+
+  crearPlato(): void {
+    const body = {
+      nombre: this.nombre,
+      precio: this.precio,
+      descripcion: this.descripcion,
+      categoria: this.categoria,
+      imagen: this.imagen,
+    };
+
+    this.platoService.crearPlato(body).subscribe({
+      next: (res) => {
+        this.router.navigate(['home']);
+      },
+      error: (err) => {
+        console.log('Error', err);
+      },
+    });
+    
+  }
+}
--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -14,16 +14,64 @@ import { CommonModule } from '@angular/common';
       <h1 class="text-center mb-4">Nuestro Menú</h1>
 
       <!-- Entradas -->
-      <div class="mb-5">
+      <div class="my-5">
         <h2 class="mb-3">Entradas</h2>
+        @for (plato of platos; track plato.categoria) { @if (plato.categoria ==
+        'Entrada') {
+        <div class="col-md-6">
+          <div class="card shadow">
+            <div class="card-body" (click)="verPlato(plato._id)">
+              <h5 class="card-title">{{ plato.nombre }}</h5>
+              <p class="card-text">
+                {{ plato.descripcion }}
+              </p>
+              <p class="fw-bold">
+                Precio:
+                {{
+                  plato.precio | currency : 'COP' : 'symbol-narrow' : '1.0-0'
+                }}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        } }
+      </div>
+
+      <!-- Platos Principales -->
+      <div class="my-5">
+        <h2 class="mb-3">Platos Fuertes</h2>
+        @for (plato of platos; track plato.categoria) { @if (plato.categoria ==
+        'Plato fuerte') {
+        <div class="col-md-6">
+          <div class="card shadow">
+            <div class="card-body" (click)="verPlato(plato._id)">
+              <h5 class="card-title">{{ plato.nombre }}</h5>
+              <p class="card-text">
+                {{ plato.descripcion }}
+              </p>
+              <p class="fw-bold">
+                Precio:
+                {{
+                  plato.precio | currency : 'COP' : 'symbol-narrow' : '1.0-0'
+                }}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        } }
+      </div>
+
+      <!-- Postres -->
+      <div class="my-5">
+        <h2 class="mb-3">Postres</h2>
         <div class="row g-4">
-          <div
-            class="col-md-6"
-            *ngFor="let plato of platos; trackById"
-            (click)="verPlato(plato._id)"
-          >
+          @for (plato of platos; track plato.categoria) { @if (plato.categoria
+          == 'Postre') {
+          <div class="col-md-6">
             <div class="card shadow">
-              <div class="card-body">
+              <div class="card-body" (click)="verPlato(plato._id)">
                 <h5 class="card-title">{{ plato.nombre }}</h5>
                 <p class="card-text">
                   {{ plato.descripcion }}
@@ -37,99 +85,33 @@ import { CommonModule } from '@angular/common';
               </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <!-- Platos Principales -->
-      <div class="mb-5">
-        <h2 class="mb-3">Platos Principales</h2>
-        <div class="row g-4">
-          <div class="col-md-6">
-            <div class="card shadow">
-              <div class="card-body">
-                <h5 class="card-title">Pasta Alfredo con Pollo</h5>
-                <p class="card-text">
-                  Pasta al dente en una cremosa salsa Alfredo, acompañada de
-                  jugosas tiras de pollo.
-                </p>
-                <p class="fw-bold">Precio: $56,900 COP</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card shadow">
-              <div class="card-body">
-                <h5 class="card-title">Lomo Saltado</h5>
-                <p class="card-text">
-                  Tiras de lomo de res salteadas con cebolla, tomate y especias,
-                  servido con papas fritas y arroz.
-                </p>
-                <p class="fw-bold">Precio: $64,900 COP</p>
-              </div>
-            </div>
-          </div>
+          } }
         </div>
-      </div>
 
-      <!-- Postres -->
-      <div class="mb-5">
-        <h2 class="mb-3">Postres</h2>
-        <div class="row g-4">
+        <!-- Bebidas -->
+        <div class="my-5">
+          <h2 class="mb-3">Bebidas</h2>
+          @for (plato of platos; track plato.categoria) { @if (plato.categoria
+          == 'Bebida') {
           <div class="col-md-6">
             <div class="card shadow">
-              <div class="card-body">
-                <h5 class="card-title">Tiramisú Clásico</h5>
+              <div class="card-body" (click)="verPlato(plato._id)">
+                <h5 class="card-title">{{ plato.nombre }}</h5>
                 <p class="card-text">
-                  Capas de bizcocho esponjoso empapadas en café, con mascarpone
-                  y un toque de cacao.
+                  {{ plato.descripcion }}
                 </p>
-                <p class="fw-bold">Precio: $22,500 COP</p>
+                <p class="fw-bold">
+                  Precio:
+                  {{
+                    plato.precio | currency : 'COP' : 'symbol-narrow' : '1.0-0'
+                  }}
+                </p>
               </div>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="card shadow">
-              <div class="card-body">
-                <h5 class="card-title">Cheesecake de Frutos Rojos</h5>
-                <p class="card-text">
-                  Tarta de queso suave, cubierta con una deliciosa salsa de
-                  frutos rojos frescos.
-                </p>
-                <p class="fw-bold">Precio: $24,900 COP</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <!-- Bebidas -->
-      <div class="mb-5">
-        <h2 class="mb-3">Bebidas</h2>
-        <div class="row g-4">
-          <div class="col-md-6">
-            <div class="card shadow">
-              <div class="card-body">
-                <h5 class="card-title">Limonada Natural</h5>
-                <p class="card-text">
-                  Refrescante limonada hecha con limones frescos y un toque de
-                  menta.
-                </p>
-                <p class="fw-bold">Precio: $8,900 COP</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card shadow">
-              <div class="card-body">
-                <h5 class="card-title">Mojito sin Alcohol</h5>
-                <p class="card-text">
-                  Una mezcla perfecta de hierbabuena, lima, soda y azúcar, ideal
-                  para refrescarse.
-                </p>
-                <p class="fw-bold">Precio: $13,900 COP</p>
-              </div>
-            </div>
-          </div>
+          } }
         </div>
       </div>
     </div>
